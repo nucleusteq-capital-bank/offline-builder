b691c9beba7fe833dcf24a1c685d7c096ab5d620
rajeshbnagaraju <rajeshnagaraju@gmail.com>
2026-03-16 16:35:38 -0700

    Added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,10 @@
 import org.gradle.api.artifacts.component.ModuleComponentIdentifier
 import org.gradle.api.artifacts.result.ResolvedArtifactResult
 import java.io.File
-import java.net.URL
-
-// ---------------- Versions ----------------
 
 val springBootVersion = "3.5.6"
-val dependencyManagementVersion = "1.1.6"
+val dependencyManagementVersion = "1.1.7"
 val sonarVersion = "5.0.0.4638"
-
-// ---------------- Repo Output ----------------
 
 val repoDir = file("offline-repo")
 
@@ -18,46 +13,33 @@ repositories {
     gradlePluginPortal()
 }
 
-// ---------------- Configuration ----------------
-
-val resolveAll by configurations.creating {
-    attributes {
-        attribute(
-            org.gradle.api.attributes.Category.CATEGORY_ATTRIBUTE,
-            objects.named(org.gradle.api.attributes.Category.LIBRARY)
-        )
-    }
-}
-
-// ---------------- Dependencies ----------------
+val resolveAll by configurations.creating
 
 dependencies {
 
-    // -------- Plugin markers --------
+    // Plugin markers
     resolveAll("org.springframework.boot:org.springframework.boot.gradle.plugin:$springBootVersion")
-    // -------- Plugin implementations --------
+    resolveAll("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:$dependencyManagementVersion")
+
+    // Plugin implementations
     resolveAll("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
     resolveAll("io.spring.gradle:dependency-management-plugin:$dependencyManagementVersion")
     resolveAll("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:$sonarVersion")
 
-    // -------- Plugin implementations --------
-    resolveAll("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
-    resolveAll("io.spring.gradle:dependency-management-plugin:$dependencyManagementVersion")
-    resolveAll("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:$sonarVersion")
-
-    // -------- Spring dependencies --------
+    // Common Spring dependencies
     resolveAll("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
     resolveAll("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
     resolveAll("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
 }
 
-// ---------------- Task ----------------
-
 tasks.register("buildOfflineRepo") {
 
     doLast {
 
-        val artifacts = resolveAll.incoming.artifacts.artifacts
+        val artifacts = resolveAll
+            .incoming
+            .artifacts
+            .artifacts
             .filterIsInstance<ResolvedArtifactResult>()
 
         artifacts.forEach {
@@ -71,20 +53,19 @@ tasks.register("buildOfflineRepo") {
             // Copy JAR
             it.file.copyTo(File(targetDir, it.file.name), overwrite = true)
 
-            // Download POM
-            val pomUrl =
-                "https://repo.maven.apache.org/maven2/$groupPath/${id.module}/${id.version}/${id.module}-${id.version}.pom"
+            // Locate POM in Gradle cache
+            val cacheDir = File(System.getProperty("user.home"))
+                .resolve(".gradle/caches/modules-2/files-2.1")
+                .resolve(id.group)
+                .resolve(id.module)
+                .resolve(id.version)
 
-            val pomFile = File(targetDir, "${id.module}-${id.version}.pom")
+            val pom = cacheDir
+                .walkTopDown()
+                .firstOrNull { f -> f.name.endsWith(".pom") }
 
-            try {
-                URL(pomUrl).openStream().use { input ->
-                    pomFile.outputStream().use { output ->
-                        input.copyTo(output)
-                    }
-                }
-            } catch (e: Exception) {
-                println("Could not download POM for ${id.module}:${id.version}")
+            if (pom != null) {
+                pom.copyTo(File(targetDir, pom.name), overwrite = true)
             }
         }
 
